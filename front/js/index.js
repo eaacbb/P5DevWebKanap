faaacd950aeabeb56ac1863e1022613279249b55
EaacBB <eaacbb@gmail.com>
2022-05-20 17:44:50 +0200

    init

---
+++ front/js/index.js
@@ -0,0 +1,34 @@
+//Récupérer l'id HTML a modifier
+
+const itemsCards = document.getElementById("items"); //Création d'une variable pour la section HTML des cartes produits
+console.log(itemsCards.innerHTML); //Vérification en log que la section HTML à bien été récupérée
+
+//Récupérer le Array des produits au format JSON avec fetch et l'afficher dans la console
+
+fetch("http://localhost:3000/api/products") //Fetch du array des données produits
+    .then(function(productsCards) {
+        if (productsCards.ok) {
+            return productsCards.json(); //Passage en JSON du array des données produits
+        }
+    })
+    .then(function(value) {
+        console.log(value); //Vérification que la valeur de la fonction contient bien le Array
+        const productsCards = value; //Création de la variable productCards contenant le resultat de la fonction (le Array)
+        console.table(productsCards); //Vérification que le Array est bien récupéré par fetch en l'affichant dans la console en format .table (Plus de visibilité que .log)
+        //Création d'une boucle pour implémenter le JSON dans l'HTML
+        for (let i = 0; i < productsCards.length; i++) {
+            itemsCards.innerHTML += `
+            <a href="./product.html?id=${productsCards[i]._id}">
+            <article>
+                <img src="${productsCards[i].imageUrl}" alt="${productsCards[i].altTxt}">
+                <h3 class="productName">${productsCards[i].name}</h3>
+                <p class="productDescription">${productsCards[i].description}</p>
+            </article>
+            </a>`
+        }
+    })
+    //Au cas où un disfonctionnement se produit, réaction à l'erreur
+    .catch(function(err) {
+        console.log("An error as occured") //Affichage dans la console du malfonctionnement
+        itemsCards.innerHTML = "An error as occured and products cannot be displayed for now, please try again later." //Affichage sur l'interface utilisateur du malfonctionnement
+    });
---
+++ front/js/product.js
@@ -0,0 +1,58 @@
+//Récupération de l'ID de l'Url de chaque produit
+(async function() {
+    const productId = getProductId();
+    console.log(productId); //Vérification que l'ID est bien récupéré
+    const product = await getProduct(productId);
+    console.log(product); //Vérification que le produit correspondant a l'ID dans le Array est bien entièrement récupéré
+    displayProduct(product);
+})();
+
+function getProductId() {
+    return new URL(location.href).searchParams.get("id"); //Récupération de l'ID avec location (utilisation de la console)
+};
+
+//Fetch des données produits du Array
+function getProduct(productId) {
+    return fetch("http://localhost:3000/api/products/" + productId)
+    .then(function(productsCards) {
+        return productsCards.json();
+    })
+    .then(function(products) {
+        return products;
+    })
+    //Au cas où un disfonctionnement se produit, réaction à l'erreur
+    .catch(function(err) {
+        console.log("An error as occured") //Affichage dans la console du malfonctionnement
+        itemsCards.innerHTML = "An error as occured and your product cannot be displayed for now, please try again later." //Affichage sur l'interface utilisateur du malfonctionnement
+    });
+}
+
+//Déclaration des variables correspondantes aux classes des DIV a implémenter en HTML
+const sectionItem = document.querySelector(".item"); //Variable crée pour afficher une erreur à l'utilisateur
+const itemImg = document.querySelector(".item__img");
+const itemTitle = document.getElementById("title");
+const itemPrice = document.getElementById("price");
+const itemDesc = document.getElementById("description");
+const itemColor = document.getElementById("colors");
+
+//Verification que les variables sont bien liées aux éléments HTML
+console.log(itemImg.innerHTML); //Fonctionne grâce a document.querySelector et pas getDocumentsByClassName
+console.log(itemTitle.innerHTML);
+console.log(itemPrice.innerHTML);
+console.log(itemDesc.innerHTML);
+console.log(itemColor.innerHTML); //Est bien liée mais necessite quelque chose en plus pour implémenter
+
+//Fonction d'implémentation des donnée du produit en HTML
+function displayProduct(product) {
+    itemImg.innerHTML = `<img src=${product.imageUrl} alt="${product.altTxt}">`; //Problème réglé avec querySelector
+    itemTitle.innerHTML = product.name;
+    itemPrice.innerHTML = product.price;
+    itemDesc.innerHTML = product.description;
+    //Déclaration de la variable colorOptions (contenu du Array "colors" provenant du Array "product")
+    const colorOptions = product.colors;
+    console.log(colorOptions); //Affichage du Array "colors" dans le Array "product"
+    //Implémentation des options dans l'HTML avec une boucle
+    for (let i = 0; i < colorOptions.length; i++) {
+        itemColor.innerHTML += `<option value="${colorOptions[i]}">${colorOptions[i]}</option>`; //Fonctionne (Nécessité absolue de créer une boucle for - ? fonctionne pas si rentrés un par un)
+    }
+}
